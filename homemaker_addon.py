0b895644105cd1317722918e52926b0a629b6c6b
Bruno Postle <bruno@postle.net>
2021-01-27 23:10:17 +0000

    Actually rename 'topologise' to 'topologist', add a simple Blender addon

---
+++ homemaker_addon.py
@@ -0,0 +1,73 @@
+import sys
+
+sys.path.append('/home/bruno/src/topologicPy/cpython')
+sys.path.append('/home/bruno/src/homemaker-addon')
+
+import topologic
+from topologic import Vertex, Face, CellComplex, Topology
+import topologist
+from topologist import *
+
+import bpy
+import bmesh
+
+bl_info = {
+    "name": "Homemaker Topologise",
+    "blender": (2, 80, 0),
+    "category": "Object",
+}
+
+class ObjectHomemaker(bpy.types.Operator):
+    """Object Homemaker Topologise"""
+    bl_idname = "object.homemaker"
+    bl_label = "Homemaker Topologise"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        for bl_object in context.selected_objects:
+
+            bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+            depsgraph = bpy.context.evaluated_depsgraph_get()
+            bl_object = bl_object.evaluated_get(depsgraph)
+            # Get a BMesh representation
+            bm = bmesh.new()   # create an empty BMesh
+            bm.from_mesh(bl_object.data)   # fill it in from a Mesh
+            bmesh.ops.remove_doubles(bm, verts=bm.verts, dist=0.001)
+            bm.verts.ensure_lookup_table()
+
+            vertices = []
+
+            for v in bm.verts:
+                coor = v.co[:]
+                vertex = Vertex.ByCoordinates(coor[0], coor[1], coor[2])
+                vertices.append(vertex)
+
+            faces = []
+
+            for f in bm.faces:
+                vertices_face = []
+                for v in f.verts:
+                    vertex = vertices[v.index]
+                    vertices_face.append(vertex)
+                face = Face.ByVertices(vertices_face)
+                faces.append(face)
+
+            cc = CellComplex.ByFaces2(faces, 0.0001)
+            output = Topology.Analyze(cc)
+            print(output)
+
+        return {'FINISHED'}
+
+def menu_func(self, context):
+    self.layout.operator(ObjectHomemaker.bl_idname)
+
+def register():
+    bpy.utils.register_class(ObjectHomemaker)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
+
+def unregister():
+    bpy.utils.unregister_class(ObjectHomemaker)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+
+if __name__ == "__main__":
+    register()
