ed831410418379dd80bcb415693b34ecf22bab4c
Bruno Postle <bruno@postle.net>
2021-01-28 07:23:12 +0000

    some more pylint suggestions

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -2,23 +2,23 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import unittest
 
-import topologic
-from topologic import Vertex, Edge, Wire, Face, Cell, CellComplex, Cluster, Topology
-import topologist
-from topologist import *
+from topologic import Vertex, Face, Cell, CellComplex, Topology
 
-points = [[0,0,0], [10,0,0], [10,10,0], [0,10,0], [0,0,10], [10,0,10], [10,10,10], [0,10,10]]
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from topologist import getSubTopologies
+
+points = [[0.0, 0.0, 0.0], [10.0, 0.0, 0.0], [10.0, 10.0, 0.0], [0.0, 10.0, 0.0],
+          [0.0, 0.0, 10.0], [10.0, 0.0, 10.0], [10.0, 10.0, 10.0], [0.0, 10.0, 10.0]]
 
 vertices = []
 for point in points:
     vertex = Vertex.ByCoordinates(point[0], point[1], point[2])
     vertices.append(vertex)
 
-faces_by_vertex_id = [[0,1,2], [0,2,3], [1,2,6,5], [2,3,7,6], [0,4,7,3], [0,1,5,4], [4,5,6], [4,6,7], [0,2,6,4]]
+faces_by_vertex_id = [[0, 1, 2], [0, 2, 3], [1, 2, 6, 5], [2, 3, 7, 6], [0, 4, 7, 3],
+                      [0, 1, 5, 4], [4, 5, 6], [4, 6, 7], [0, 2, 6, 4]]
 
 faces = []
 for face_by_id in faces_by_vertex_id:
@@ -34,14 +34,14 @@ cc = CellComplex.ByFaces2(faces, 0.0001)
 class Tests(unittest.TestCase):
     def test_vertices(self):
         self.assertEqual(len(points), len(vertices))
-        self.assertEqual(points[0][0], 0)
-        self.assertEqual(points[1][0], 10)
-        self.assertEqual(vertices[0].X(), 0)
-        self.assertEqual(vertices[1].X(), 10)
-        self.assertEqual(vertices[-1].Y(), 10)
+        self.assertEqual(points[0][0], 0.0)
+        self.assertEqual(points[1][0], 10.0)
+        self.assertEqual(vertices[0].X(), 0.0)
+        self.assertEqual(vertices[1].X(), 10.0)
+        self.assertEqual(vertices[-1].Y(), 10.0)
     def test_faces(self):
         self.assertEqual(len(faces_by_vertex_id), len(faces))
-        self.assertEqual(faces_by_vertex_id[0][2], 2)
+        self.assertEqual(faces_by_vertex_id[0][2], 2.0)
         self.assertTrue(faces[0].IsHorizontal())
         self.assertFalse(faces[0].IsVertical())
         self.assertTrue(faces[2].IsVertical())
@@ -62,6 +62,7 @@ class Tests(unittest.TestCase):
         self.assertEqual(len(cells), 2)
         centroid_0 = cells[0].Centroid()
         centroid_1 = cells[1].Centroid()
+        self.assertEqual(centroid_0.Z(), 5.0)
         self.assertEqual(centroid_1.Z(), 5.0)
         volume_0 = cells[0].Volume()
         volume_1 = cells[1].Volume()
