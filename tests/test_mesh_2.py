9462b2d7a01ec1f393e4207cc8e327d0ba827e34
Bruno Postle <bruno@postle.net>
2021-01-30 18:45:23 +0000

    Fix CellsAbove() and CellsBelow() methods

---
+++ tests/test_mesh_2.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import unittest
+
+from topologic import Vertex, Face, Cell, CellComplex, Topology
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from topologist.helpers import create_stl_list
+
+points = [[0.0, 0.0, 0.0], [10.0, 0.0, 0.0], [10.0, 10.0, 0.0], [0.0, 10.0, 0.0],
+          [0.0, 0.0, 10.0], [10.0, 0.0, 10.0], [10.0, 10.0, 10.0], [0.0, 10.0, 10.0]]
+
+points.extend([[0.0, 0.0, 20.0], [10.0, 0.0, 20.0], [10.0, 10.0, 20.0], [0.0, 10.0, 20.0]])
+
+vertices = []
+for point in points:
+    vertex = Vertex.ByCoordinates(point[0], point[1], point[2])
+    vertices.append(vertex)
+
+faces_by_vertex_id = [[0, 1, 2], [0, 2, 3], [1, 2, 6, 5], [2, 3, 7, 6], [0, 4, 7, 3],
+                      [0, 1, 5, 4], [4, 5, 6], [4, 6, 7], [0, 2, 6, 4]]
+
+faces_by_vertex_id.extend([[4, 5, 9, 8], [5, 6, 10, 9], [6, 7, 11, 10], [7, 4, 8, 11],
+                          [8, 9, 10, 11]])
+
+faces = []
+for face_by_id in faces_by_vertex_id:
+    vertices_face = []
+    for point_id in face_by_id:
+        vertex = vertices[point_id]
+        vertices_face.append(vertex)
+    face_by_vertices = Face.ByVertices(vertices_face)
+    faces.append(face_by_vertices)
+
+cc = CellComplex.ByFaces2(faces, 0.0001)
+
+class Tests(unittest.TestCase):
+    """14 faces and three cells formed by a cube sliced on the diagonal"""
+    def test_faces_cc(self):
+
+        faces_all = create_stl_list(Face)
+        cc.Faces(faces_all)
+        self.assertEqual(len(faces_all), 14)
+        for face in faces_all:
+            cells = create_stl_list(Cell)
+            face.Cells(cells)
+            self.assertGreater(len(cells), 0)
+            self.assertLess(len(cells), 3)
+
+        faces_vertical = create_stl_list(Face)
+        cc.FacesVertical(faces_vertical)
+        self.assertEqual(len(faces_vertical), 9)
+        for face in faces_vertical:
+            self.assertTrue(face.IsVertical())
+
+        faces_horizontal = create_stl_list(Face)
+        cc.FacesHorizontal(faces_horizontal)
+        self.assertEqual(len(faces_horizontal), 5)
+        for face in faces_horizontal:
+            self.assertTrue(face.IsHorizontal())
+
+    def test_cells(self):
+
+        centroid = cc.Centroid()
+        self.assertEqual(centroid.X(), 5.0)
+        self.assertEqual(centroid.Y(), 5.0)
+        self.assertEqual(centroid.Z(), 10.0)
+
+        cells = create_stl_list(Cell)
+        cc.Cells(cells)
+        self.assertEqual(len(cells), 3)
+
+        for cell in cells:
+            centroid = cell.Centroid()
+            volume = cell.Volume()
+
+            faces_vertical = create_stl_list(Face)
+            cell.FacesVertical(faces_vertical)
+
+            faces_horizontal = create_stl_list(Face)
+            cell.FacesHorizontal(faces_horizontal)
+
+            cells_above = create_stl_list(Cell)
+            cell.CellsAbove(cc, cells_above)
+            if len(cells_above) == 1:
+                self.assertAlmostEqual(cell.Volume(), 500.0)
+            elif len(cells_above) == 0:
+                self.assertAlmostEqual(cell.Volume(), 1000.0)
+
+            cells_below = create_stl_list(Cell)
+            cell.CellsBelow(cc, cells_below)
+            if len(cells_below) == 2:
+                self.assertAlmostEqual(cell.Volume(), 1000.0)
+            elif len(cells_below) == 0:
+                self.assertAlmostEqual(cell.Volume(), 500.0)
+
+output = Topology.Analyze(cc)
+#print(output)
+
+if __name__ == '__main__':
+    unittest.main()
--- topologist/cell.py
+++ topologist/cell.py
@@ -1,5 +1,5 @@
 import topologic
-from topologic import Face, Cell, CellUtility
+from topologic import Face, Cell, FaceUtility, CellUtility
 from topologist.helpers import create_stl_list
 
 def Volume(self):
@@ -19,24 +19,26 @@ def FacesBottom(self, faces_result):
         if(face.Elevation() == self.Elevation() and face.Height() == 0.0):
             faces_result.push_back(face)
 
-def CellsAbove(self, cells_result):
+def CellsAbove(self, topology, cells_result):
     """Cells (excluding self) connected to top faces of this cell"""
-    faces_top = self.FacesTop()
-    elements_ptr = create_stl_list(Cell)
-    self.Cells(elements_ptr)
-    for cell in elements_ptr:
-        if not cell is self:
-            if cell in faces_top:
+    faces_top = create_stl_list(Face)
+    self.FacesTop(faces_top)
+    for face in faces_top:
+        cells = create_stl_list(Cell)
+        FaceUtility.AdjacentCells(face, topology, cells)
+        for cell in cells:
+            if not cell.IsSame(self):
                 cells_result.push_back(cell)
 
-def CellsBelow(self, cells_result):
+def CellsBelow(self, topology, cells_result):
     """Cells (excluding self) connected to bottom faces of this cell"""
-    faces_bottom = self.FacesBottom()
-    elements_ptr = create_stl_list(Cell)
-    self.Cells(elements_ptr)
-    for cell in elements_ptr:
-        if not cell is self:
-            if cell in faces_bottom:
+    faces_bottom = create_stl_list(Face)
+    self.FacesBottom(faces_bottom)
+    for face in faces_bottom:
+        cells = create_stl_list(Cell)
+        FaceUtility.AdjacentCells(face, topology, cells)
+        for cell in cells:
+            if not cell.IsSame(self):
                 cells_result.push_back(cell)
 
 def IsOutside(self):
