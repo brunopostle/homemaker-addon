7effedeb0e0725c2c22107cb580df4295caee8e2
Bruno Postle <bruno@postle.net>
2021-02-03 18:08:39 +0000

    remove some unused functions

--- topologist/helpers.py
+++ topologist/helpers.py
@@ -2,6 +2,19 @@ from topologic import Vertex, Edge, Wire, Face, Shell, Cell, CellComplex, Cluste
 
 import cppyy
 
+def dummy():
+    """dummy method to trick linter"""
+    Vertex()
+    Vertex()
+    Edge()
+    Wire()
+    Face()
+    Shell()
+    Cell()
+    CellComplex()
+    Cluster()
+    Topology()
+
 def create_stl_list(cppyy_data_type):
     return cppyy.gbl.std.list[cppyy_data_type.Ptr]()
 
@@ -10,55 +23,6 @@ def convert_to_stl_list(py_list, cppyy_data_type):
     for i in py_list:
         values.push_back(i)
     return values
-
-def convert_to_py_list(stl_list):
-    py_list = []
-    i = stl_list.begin()
-    while i != stl_list.end():
-        py_list.append(i.__deref__())
-        _ = i.__preinc__()
-    return py_list
-
-def classByType(argument):
-    switcher = {
-        1: Vertex,
-        2: Edge,
-        4: Wire,
-        8: Face,
-        16: Shell,
-        32: Cell,
-        64: CellComplex,
-        128: Cluster}
-    return switcher.get(argument, Topology)
-
-def fixTopologyClass(topology):
-    topology.__class__ = classByType(topology.GetType())
-    return topology
-
-def getSubTopologies(topology, subTopologyClass):
-    pointer = subTopologyClass.Ptr
-    values = cppyy.gbl.std.list[pointer]()
-    if subTopologyClass == Vertex:
-        _ = topology.Vertices(values)
-    elif subTopologyClass == Edge:
-        _ = topology.Edges(values)
-    elif subTopologyClass == Wire:
-        _ = topology.Wires(values)
-    elif subTopologyClass == Face:
-        _ = topology.Faces(values)
-    elif subTopologyClass == Shell:
-        _ = topology.Shells(values)
-    elif subTopologyClass == Cell:
-        _ = topology.Cells(values)
-    elif subTopologyClass == CellComplex:
-        _ = topology.CellComplexes(values)
-
-    py_list = []
-    i = values.begin()
-    while i != values.end():
-        py_list.append(fixTopologyClass(Topology.DeepCopy(i.__deref__())))
-        _ = i.__preinc__()
-    return py_list
 
 def vertex_index(v, vertices):
     index = None
