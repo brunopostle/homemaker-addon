a5e9b983d3a907f0531c1ab3faedc2243311d854
Bruno Postle <bruno@postle.net>
2021-02-09 23:59:45 +0000

    Cell.Perimeter() method

--- topologist/cell.py
+++ topologist/cell.py
@@ -1,5 +1,5 @@
 import topologic
-from topologic import Face, Cell, FaceUtility
+from topologic import Vertex, Edge, Wire, Face, Cell, FaceUtility
 from topologist.helpers import create_stl_list
 
 def FacesTop(self, faces_result):
@@ -14,6 +14,13 @@ def FacesBottom(self, faces_result):
     self.Faces(elements_ptr)
     for face in elements_ptr:
         if(face.Elevation() == self.Elevation() and face.Height() == 0.0):
+            faces_result.push_back(face)
+
+def FacesVertical(self, faces_result):
+    elements_ptr = create_stl_list(Face)
+    self.Faces(elements_ptr)
+    for face in elements_ptr:
+        if face.IsVertical():
             faces_result.push_back(face)
 
 def FacesVerticalExternal(self, faces_result):
@@ -73,11 +80,36 @@ def Crinkliness(self):
 
 def Perimeter(self):
     """2D outline of cell floor, closed, anti-clockwise"""
-    # TODO
-    pass
+    faces = create_stl_list(Face)
+    self.FacesVertical(faces)
+    edges = create_stl_list(Edge)
+    for face in faces:
+        edges.push_back(face.AxisOuter())
+    floor = Face.ByEdges(edges)
+    normal = floor.Normal()
+
+    # FIXME should support doughnut shaped rooms
+    wires = create_stl_list(Wire)
+    floor.Wires(wires)
+    for wire in wires:
+        vertices = create_stl_list(Vertex)
+        wire.Vertices(vertices)
+        if normal.Z() > 0.9999:
+            # good, loop is anticlockwise
+            return vertices
+        elif normal.Z() < -0.9999:
+            vertices_reversed = create_stl_list(Vertex)
+            vertices_list = list(vertices)
+            vertices_list.reverse()
+            for vertex in vertices_list:
+                vertices_reversed.push_back(vertex)
+            return vertices_reversed
+        else:
+            return None
 
 setattr(topologic.Cell, 'FacesTop', FacesTop)
 setattr(topologic.Cell, 'FacesBottom', FacesBottom)
+setattr(topologic.Cell, 'FacesVertical', FacesVertical)
 setattr(topologic.Cell, 'FacesVerticalExternal', FacesVerticalExternal)
 setattr(topologic.Cell, 'CellsAbove', CellsAbove)
 setattr(topologic.Cell, 'CellsBelow', CellsBelow)
