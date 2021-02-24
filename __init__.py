398a1b2162d779cf30e9ff362ebced3a57d202af
Bruno Postle <bruno@postle.net>
2021-02-24 19:23:10 +0000

    process internal/external_unsupported chains, rename some variables

--- __init__.py
+++ __init__.py
@@ -106,16 +106,16 @@ class ObjectHomemaker(bpy.types.Operator):
             walls = cc.Walls()
 
             # external walls
-            walls_external = walls['external']
-            for elevation in walls_external:
-                for height in walls_external[elevation]:
-                    for chain in walls_external[elevation][height]:
+            chains = walls['external']
+            for elevation in chains:
+                for height in chains[elevation]:
+                    for chain in chains[elevation][height]:
                         closed = 0
                         if chain.is_simple_cycle(): closed = 1
                         path = []
                         for node in chain.nodes():
                             path.append(string_to_coor_2d(node))
-                        wall = Wall({'closed': closed,
+                        part = Wall({'closed': closed,
                                        'path': path,
                                        'name': 'exterior',
                                   'elevation': elevation,
@@ -123,20 +123,38 @@ class ObjectHomemaker(bpy.types.Operator):
                                   'extension': 0.25,
                                       'level': elevations[elevation]})
 
-                        for segment in range(len(wall.openings)):
+                        for segment in range(len(part.openings)):
                             interior_type = 'Living' # FIXME
-                            wall.populate_exterior_openings(segment, interior_type, 0)
-                        molior.append(wall.__dict__)
+                            part.populate_exterior_openings(segment, interior_type, 0)
+                        molior.append(part.__dict__)
 
-            # internal walls
-            walls_internal = walls['internal']
-            for elevation in walls_internal:
-                for height in walls_internal[elevation]:
-                    for chain in walls_internal[elevation][height]:
+            # external walls unsupported
+            chains = walls['external_unsupported']
+            for elevation in chains:
+                for height in chains[elevation]:
+                    for chain in chains[elevation][height]:
+                        closed = 0
+                        if chain.is_simple_cycle(): closed = 1
                         path = []
                         for node in chain.nodes():
                             path.append(string_to_coor_2d(node))
-                        wall = Wall({'closed': 0,
+                        part = Extrusion({'closed': closed,
+                                            'path': path,
+                                            'name': 'ground beam',
+                                       'elevation': elevation,
+                                          'height': 0.0,
+                                           'level': elevations[elevation]})
+                        molior.append(part.__dict__)
+
+            # internal walls
+            chains = walls['internal']
+            for elevation in chains:
+                for height in chains[elevation]:
+                    for chain in chains[elevation][height]:
+                        path = []
+                        for node in chain.nodes():
+                            path.append(string_to_coor_2d(node))
+                        part = Wall({'closed': 0,
                                        'path': path,
                                        'name': 'interior',
                                   'elevation': elevation,
@@ -146,17 +164,33 @@ class ObjectHomemaker(bpy.types.Operator):
                                      'height': height,
                                   'extension': 0.0,
                                       'level': elevations[elevation]})
-                        for segment in range(len(wall.openings)):
+                        for segment in range(len(part.openings)):
                             type_a = 'Living' # FIXME
                             type_b = 'Living' # FIXME
-                            wall.populate_interior_openings(segment, type_a, type_b, 0)
-                        molior.append(wall.__dict__)
+                            part.populate_interior_openings(segment, type_a, type_b, 0)
+                        molior.append(part.__dict__)
+
+            # internal walls unsupported
+            chains = walls['internal_unsupported']
+            for elevation in chains:
+                for height in chains[elevation]:
+                    for chain in chains[elevation][height]:
+                        path = []
+                        for node in chain.nodes():
+                            path.append(string_to_coor_2d(node))
+                        part = Extrusion({'closed': 0,
+                                            'path': path,
+                                            'name': 'ground beam',
+                                       'elevation': elevation,
+                                          'height': 0.0,
+                                           'level': elevations[elevation]})
+                        molior.append(part.__dict__)
 
             # eaves
-            walls_eaves = walls['eaves']
-            for elevation in walls_eaves:
-                for height in walls_eaves[elevation]:
-                    for chain in walls_eaves[elevation][height]:
+            chains = walls['eaves']
+            for elevation in chains:
+                for height in chains[elevation]:
+                    for chain in chains[elevation][height]:
                         closed = 0
                         name = 'parapet'
                         if chain.is_simple_cycle():
@@ -165,30 +199,30 @@ class ObjectHomemaker(bpy.types.Operator):
                         path = []
                         for node in chain.nodes():
                             path.append(string_to_coor_2d(node))
-                        wall = Extrusion({'closed': closed,
+                        part = Extrusion({'closed': closed,
                                             'path': path,
                                             'name': name,
                                        'elevation': elevation,
                                            'level': elevations[elevation]})
-                        molior.append(wall.__dict__)
+                        molior.append(part.__dict__)
 
             # open walls, where both sides are 'outside'
-            walls_open = walls['open']
-            for elevation in walls_open:
-                for height in walls_open[elevation]:
-                    for chain in walls_open[elevation][height]:
+            chains = walls['open']
+            for elevation in chains:
+                for height in chains[elevation]:
+                    for chain in chains[elevation][height]:
                         closed = 0
                         if chain.is_simple_cycle(): closed = 1
                         path = []
                         for node in chain.nodes():
                             path.append(string_to_coor_2d(node))
-                        wall = Extrusion({'closed': closed,
+                        part = Extrusion({'closed': closed,
                                             'path': path,
-                                            'name': 'external-beam',
+                                            'name': 'external beam',
                                           'height': height,
                                        'elevation': elevation,
                                            'level': elevations[elevation]})
-                        molior.append(wall.__dict__)
+                        molior.append(part.__dict__)
 
             # rooms
             cells = create_stl_list(Cell)
