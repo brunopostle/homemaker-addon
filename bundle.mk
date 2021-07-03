f464cfbcf3fdcca4c16e350591c452f1eb891a20
Bruno Postle <bruno@postle.net>
2021-07-03 09:24:58 +0000

    roll-back binary package from cppyy-2.0.0 to cppyy-1.9.6

--- bundle.mk
+++ bundle.mk
@@ -11,10 +11,12 @@ dist:
 	rm -rf dist/homemaker/*/*/__pycache__
 	mkdir -p dist/working
 
-	cd dist/working && wget https://files.pythonhosted.org/packages/94/0b/960513ec1b582793e92dafb7915c6498d688fd619f79691916b84f2f3bee/cppyy_cling-6.25.0-py2.py3-none-manylinux2014_x86_64.whl
-	cd dist/working && wget https://files.pythonhosted.org/packages/85/15/f7a4b706c6b91045ee3c865ae593ddf077a463914aadddbe803c0c11992a/cppyy-backend-1.14.5.tar.gz
-	cd dist/working && wget https://files.pythonhosted.org/packages/ed/a2/75e715ec671bd491729fb208b9757239148b7efe02d801032eab30c19846/CPyCppyy-1.12.6.tar.gz
-	cd dist/working && wget https://files.pythonhosted.org/packages/81/3d/cf40833ecb8f2ae7024a8aeead8d5f699c3350cbf57582f087900ca0dddf/cppyy-2.0.0.tar.gz
+        # roll-back to 29th April 2021 release (see #4)
+	cd dist/working && wget https://files.pythonhosted.org/packages/51/8c/378b48e588359832b6a412cc63527a6af9fe797a92c6b726cedd442df7e8/cppyy_cling-6.21.7-py2.py3-none-manylinux2014_x86_64.whl
+	cd dist/working && wget https://files.pythonhosted.org/packages/83/34/1d4a4a51909c2320c100e1d2aff47dbcb9efe3dfe4e1c150025a07027bfd/cppyy-backend-1.14.4.tar.gz
+	cd dist/working && wget https://files.pythonhosted.org/packages/fb/cc/ca43218f5b8768e34a881605dcb899f6efe017915eacbc1c85de665f0864/CPyCppyy-1.12.5.tar.gz
+	cd dist/working && wget https://files.pythonhosted.org/packages/ee/7e/ee06951cc63c14e708357f6d9ee1a9eff6cdf48b5d51e4de87db9c6ba071/cppyy-1.9.6.tar.gz
+
 	cd dist/working && wget https://files.pythonhosted.org/packages/ac/dd/f6fc54a770ba0222261b33d60d9c9e01aa35d989f1cdfe892ae84e319779/ezdxf-0.16.3-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl
 	cd dist/working && wget https://files.pythonhosted.org/packages/8a/bb/488841f56197b13700afd5658fc279a2025a39e22449b7cf29864669b15d/pyparsing-2.4.7-py2.py3-none-any.whl
 	cd dist/working && wget https://github.com/brunopostle/Topologic/releases/download/2021-06-15/TopologicCore-2021-06-15-manylinux_2_24-x86_64.zip
@@ -39,16 +41,16 @@ dist:
 	cp -r dist/working/cppyy_backend dist/homemaker/libs/site/packages/
 
 	cd dist/working && tar -xzvf cppyy-backend-*.tar.gz
-	cd dist/working/cppyy-backend-1.14.5/ && PYTHONPATH=../../homemaker/libs/site/packages python setup.py build && cp -r build/lib.*/cppyy_backend/lib/* ../../homemaker/libs/site/packages/cppyy_backend/lib/
+	cd dist/working/cppyy-backend-1.14.4/ && PYTHONPATH=../../homemaker/libs/site/packages python setup.py build && cp -r build/lib.*/cppyy_backend/lib/* ../../homemaker/libs/site/packages/cppyy_backend/lib/
 
 	cd dist/working && tar -xzvf CPyCppyy-*.tar.gz
-	cd dist/working/CPyCppyy-1.12.6/ && PYTHONPATH=../../homemaker/libs/site/packages python setup.py build && cp -r build/lib.*/* ../../homemaker/libs/site/packages/ && cp -r include/CPyCppyy ../../homemaker/libs/site/packages/topologic/include/api/
+	cd dist/working/CPyCppyy-1.12.5/ && PYTHONPATH=../../homemaker/libs/site/packages python setup.py build && cp -r build/lib.*/* ../../homemaker/libs/site/packages/ && cp -r include/CPyCppyy ../../homemaker/libs/site/packages/topologic/include/api/
 
 	# set rpath to find bundled libpython
 	patchelf --set-rpath '$$ORIGIN/.' dist/homemaker/libs/site/packages/libcppyy.*.so
 
-	cd dist/working && tar -xzvf cppyy-2.0.0.tar.gz
-	cd dist/working/cppyy-2.0.0/ && PYTHONPATH=../../homemaker/libs/site/packages python setup.py build && cp -r build/lib/cppyy ../../homemaker/libs/site/packages/
+	cd dist/working && tar -xzvf cppyy-1.9.6.tar.gz
+	cd dist/working/cppyy-1.9.6/ && PYTHONPATH=../../homemaker/libs/site/packages python setup.py build && cp -r build/lib/cppyy ../../homemaker/libs/site/packages/
 
 	# libpython needed with 3.7, but not 3.9 ??!?
 	if [ -e /usr/local/lib/libpython3.7m.so.1.0 ]; then cp /usr/local/lib/libpython3.7m.so.1.0 dist/homemaker/libs/site/packages/; fi
