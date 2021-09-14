139b1baf77716d183ec6ada962e3b4dbde70b850
Bruno Postle <bruno@postle.net>
2021-09-14 21:35:19 +0100

    blender operator that just processes cellcomplex

--- __init__.py
+++ __init__.py
@@ -22,7 +22,77 @@ bl_info = {
     "category": "Object",
 }
 
-# TODO operator that just generates cellcomplex geometry
+
+class ObjectTopologise(bpy.types.Operator):
+    """Object Topologise"""
+
+    bl_idname = "object.topologise"
+    bl_label = "Topologise"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+        meshes = []
+
+        for blender_object in context.selected_objects:
+            if not blender_object.type == "MESH":
+                continue
+            # Ignore widgets (if any) for allocating room/space usage
+            label = re.match(
+                "(bedroom|circulation|circulation_stair|stair|kitchen|living|retail|sahn|toilet)",
+                blender_object.name,
+                flags=re.IGNORECASE,
+            )
+            if label:
+                continue
+            else:
+                meshes.append(blender_object)
+
+        # remaining meshes become a single cellcomplex
+        faces_ptr = create_stl_list(Face)
+        for mesh in meshes:
+            vertices = [Vertex.ByCoordinates(*v.co) for v in mesh.data.vertices]
+
+            for polygon in mesh.data.polygons:
+                if polygon.area < 0.00001:
+                    continue
+                stylename = "default"
+                if len(mesh.material_slots) > 0:
+                    stylename = mesh.material_slots[
+                        polygon.material_index
+                    ].material.name
+                face_ptr = Face.ByVertices([vertices[v] for v in polygon.vertices])
+                face_ptr.Set("stylename", stylename)
+                faces_ptr.push_back(face_ptr)
+            mesh.hide_viewport = True
+
+        # Generate a Topologic CellComplex
+        cc = CellComplex.ByFaces(faces_ptr, 0.0001)
+        stl_faces = create_stl_list(Face)
+        cc.Faces(stl_faces)
+        vertices = []
+        faces = []
+        materials = []
+        vertex_id = 0
+        for stl_face in stl_faces:
+            stl_vertices = create_stl_list(Vertex)
+            stl_face.VerticesPerimeter(stl_vertices)
+            face = []
+            for vertex in list(stl_vertices):
+                vertices.append([vertex.X(), vertex.Y(), vertex.Z()])
+                face.append(vertex_id)
+                vertex_id += 1
+            faces.append(face)
+            materials.append(stl_face.Get("stylename"))
+
+        new_mesh = bpy.data.meshes.new("faces")
+        new_mesh.from_pydata(vertices, [], faces)
+        # FIXME reapply materials
+        new_mesh.update()
+        new_object = bpy.data.objects.new("cellcomplex", new_mesh)
+        bpy.data.collections[0].objects.link(new_object)
+
+        return {"FINISHED"}
 
 
 class ObjectHomemaker(bpy.types.Operator):
@@ -131,15 +201,18 @@ class ObjectHomemaker(bpy.types.Operator):
 
 
 def menu_func(self, context):
+    self.layout.operator(ObjectTopologise.bl_idname)
     self.layout.operator(ObjectHomemaker.bl_idname)
 
 
 def register():
+    bpy.utils.register_class(ObjectTopologise)
     bpy.utils.register_class(ObjectHomemaker)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
 
 def unregister():
+    bpy.utils.unregister_class(ObjectTopologise)
     bpy.utils.unregister_class(ObjectHomemaker)
     bpy.types.VIEW3D_MT_object.remove(menu_func)
 
