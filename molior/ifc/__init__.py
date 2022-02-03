brunopostle/homemaker-addon

commit 345af161e84e28c948bcbef705aecfa39b784379

allow multiple Building entities in a Project

molior/ifc/__init__.py
@@ -334,11 +334,12 @@ def create_tessellation_from_mesh(self, vertices, faces):
     return self.createIfcPolygonalFaceSet(pointlist, None, indexedfaces, None)
 
 
-def assign_storey_byindex(self, entity, index):
+def assign_storey_byindex(self, entity, building, index):
     """Assign object to a storey by index"""
     storeys = {}
     for storey in self.by_type("IfcBuildingStorey"):
-        storeys[storey.Name] = storey
+        if get_building(storey) == building:
+            storeys[storey.Name] = storey
     if entity.is_a("IfcSpatialElement"):
         run(
             "aggregate.assign_object",
@@ -355,13 +356,16 @@ def assign_storey_byindex(self, entity, index):
         )
 
 
-def assign_space_byindex(self, entity, index):
+def assign_space_byindex(self, entity, building, index):
     """Assign object to a Space by index"""
     spaces = {}
     for space in self.by_type("IfcSpace"):
-        pset_topology = ifcopenshell.util.element.get_psets(space).get("EPset_Topology")
-        if pset_topology:
-            spaces[pset_topology["CellIndex"]] = space
+        if get_building(space) == building:
+            pset_topology = ifcopenshell.util.element.get_psets(space).get(
+                "EPset_Topology"
+            )
+            if pset_topology:
+                spaces[pset_topology["CellIndex"]] = space
     if not str(index) in spaces:
         return
     if entity.is_a("IfcSpatialElement"):
@@ -457,6 +461,25 @@ def get_library_by_name(self, library_name):
         relating_context=self.by_type("IfcProject")[0],
     )
     return library
+
+
+def get_building(entity):
+    """Retrieve whatever Building contains this entity, or None"""
+    if entity.is_a("IfcElement"):
+        parents = entity.ContainedInStructure
+        if not parents:
+            return None
+        parent = parents[0]
+    elif entity.is_a("IfcSpatialElement"):
+        decomposes = entity.Decomposes
+        if not decomposes:
+            return None
+        parent = decomposes[0].RelatingObject
+    else:
+        return None
+    if parent.is_a("IfcBuilding"):
+        return parent
+    get_building(parent)
 
 
 def get_material_by_name(self, subcontext, material_name, style_materials):

__init__.py
@@ -8,7 +8,6 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/libs/site/packages
 
 from topologic import Vertex, Face, CellComplex
 from molior import Molior
-import molior.ifc
 
 import logging
 from blenderbim.bim import import_ifc
@@ -70,15 +69,6 @@ class ObjectHomemaker(bpy.types.Operator):
             faces_ptr = topologic_faces_from_blender_object(blender_object)
             blender_object.hide_viewport = True
 
-            # generate an ifcopenshell model
-            # TODO styles are loaded from share_dir, allow blender user to set custom share_dir path
-            ifc = homemaker(
-                faces_ptr=faces_ptr,
-                widgets=widgets,
-                name=blender_object.name,
-                share_dir="share",
-            )
-
             # delete any IfcProject/* collections, but leave IfcStore intact
             have_project = False
             for collection_name in bpy.data.collections.keys():
@@ -93,11 +83,18 @@ class ObjectHomemaker(bpy.types.Operator):
                 blenderbim.bim.ifc.IfcStore.purge()
                 blenderbim.bim.ifc.IfcStore.file = None
 
-            # merge IfcStore with generated ifcopenshell model
-            if blenderbim.bim.ifc.IfcStore.file == None:
-                blenderbim.bim.ifc.IfcStore.file = ifc
-            else:
-                molior.ifc.merge_file(blenderbim.bim.ifc.IfcStore.file, ifc)
+            # generate an ifcopenshell model
+            # TODO styles are loaded from share_dir, allow blender user to set custom share_dir path
+            file = homemaker(
+                file=blenderbim.bim.ifc.IfcStore.file,
+                faces_ptr=faces_ptr,
+                widgets=widgets,
+                name=blender_object.name,
+                share_dir="share",
+            )
+
+            # no idea why we have to reassign this
+            blenderbim.bim.ifc.IfcStore.file = file
 
             # (re)build blender collections and geometry from IfcStore
             ifc_import_settings = import_ifc.IfcImportSettings.factory(
@@ -110,7 +107,9 @@ class ObjectHomemaker(bpy.types.Operator):
         return {"FINISHED"}
 
 
-def homemaker(faces_ptr=[], widgets=[], name="My Building", share_dir="share"):
+def homemaker(
+    file=None, faces_ptr=[], widgets=[], name="My Building", share_dir="share"
+):
     # Generate a Topologic CellComplex
     cc = CellComplex.ByFaces(faces_ptr, 0.0001)
     # Give every Cell and Face an index number
@@ -133,6 +132,7 @@ def homemaker(faces_ptr=[], widgets=[], name="My Building", share_dir="share"):
     hulls = cc.GetHulls()
 
     molior_object = Molior(
+        file=file,
         circulation=circulation,
         traces=traces,
         hulls=hulls,

molior/__init__.py
@@ -49,6 +49,7 @@ from molior.ifc import (
     create_storeys,
     assign_space_byindex,
     assign_storey_byindex,
+    get_building,
     create_tessellation_from_mesh,
 )
 import topologist.ushell as ushell
@@ -62,6 +63,7 @@ class Molior:
 
     def __init__(self, **args):
         self.file = None
+        self.building = None
         self.traces = {}
         self.hulls = {}
         self.normals = {}
@@ -475,7 +477,7 @@ class Molior:
                 level = 0
                 if elevation in self.elevations:
                     level = self.elevations[elevation]
-                assign_storey_byindex(self.file, element, level)
+                assign_storey_byindex(self.file, element, self.building, level)
                 shape = self.file.createIfcShapeRepresentation(
                     body_context,
                     body_context.ContextIdentifier,
@@ -510,21 +512,30 @@ class Molior:
                             coor[2] - storey_elevation,
                         )
 
-        # attach Slab elements to relevant Space
+        # molior.floor attaches Slab elements directly to Storey, re-attach to relevant Space
         for element in self.file.by_type("IfcSlab"):
-            pset_topology = ifcopenshell.util.element.get_psets(element).get(
-                "EPset_Topology"
-            )
-            if pset_topology:
-                assign_space_byindex(self.file, element, pset_topology["CellIndex"])
+            if get_building(element) == self.building:
+                pset_topology = ifcopenshell.util.element.get_psets(element).get(
+                    "EPset_Topology"
+                )
+                if pset_topology:
+                    assign_space_byindex(
+                        self.file, element, self.building, pset_topology["CellIndex"]
+                    )
 
         # attach Window elements to relevant Space
         for element in self.file.by_type("IfcWindow"):
-            pset_topology = ifcopenshell.util.element.get_psets(element).get(
-                "EPset_Topology"
-            )
-            if pset_topology:
-                assign_space_byindex(self.file, element, pset_topology["BackCellIndex"])
+            if get_building(element) == self.building:
+                pset_topology = ifcopenshell.util.element.get_psets(element).get(
+                    "EPset_Topology"
+                )
+                if pset_topology:
+                    assign_space_byindex(
+                        self.file,
+                        element,
+                        self.building,
+                        pset_topology["BackCellIndex"],
+                    )
 
         cell_lookup = {}
         cells_ptr = []
@@ -534,36 +545,53 @@ class Molior:
 
         # attach Door elements to Space
         for element in self.file.by_type("IfcDoor"):
-            pset_topology = ifcopenshell.util.element.get_psets(element).get(
-                "EPset_Topology"
-            )
-            if pset_topology:
-                if not "FrontCellIndex" in pset_topology or (
-                    "FrontCellIndex" in pset_topology
-                    and cell_lookup[pset_topology["FrontCellIndex"]].IsOutside()
-                ):
-                    assign_space_byindex(
-                        self.file, element, pset_topology["BackCellIndex"]
-                    )
-                elif cell_lookup[pset_topology["BackCellIndex"]].IsOutside():
-                    assign_space_byindex(
-                        self.file, element, pset_topology["FrontCellIndex"]
-                    )
-                else:
-                    if cell_lookup[pset_topology["FrontCellIndex"]].Get(
-                        "separation"
-                    ) and float(
-                        cell_lookup[pset_topology["FrontCellIndex"]].Get("separation")
-                    ) > float(
-                        cell_lookup[pset_topology["BackCellIndex"]].Get("separation")
+            if get_building(element) == self.building:
+                pset_topology = ifcopenshell.util.element.get_psets(element).get(
+                    "EPset_Topology"
+                )
+                if pset_topology:
+                    if not "FrontCellIndex" in pset_topology or (
+                        "FrontCellIndex" in pset_topology
+                        and cell_lookup[pset_topology["FrontCellIndex"]].IsOutside()
                     ):
                         assign_space_byindex(
-                            self.file, element, pset_topology["FrontCellIndex"]
+                            self.file,
+                            element,
+                            self.building,
+                            pset_topology["BackCellIndex"],
+                        )
+                    elif cell_lookup[pset_topology["BackCellIndex"]].IsOutside():
+                        assign_space_byindex(
+                            self.file,
+                            element,
+                            self.building,
+                            pset_topology["FrontCellIndex"],
                         )
                     else:
-                        assign_space_byindex(
-                            self.file, element, pset_topology["BackCellIndex"]
-                        )
+                        if cell_lookup[pset_topology["FrontCellIndex"]].Get(
+                            "separation"
+                        ) and float(
+                            cell_lookup[pset_topology["FrontCellIndex"]].Get(
+                                "separation"
+                            )
+                        ) > float(
+                            cell_lookup[pset_topology["BackCellIndex"]].Get(
+                                "separation"
+                            )
+                        ):
+                            assign_space_byindex(
+                                self.file,
+                                element,
+                                self.building,
+                                pset_topology["FrontCellIndex"],
+                            )
+                        else:
+                            assign_space_byindex(
+                                self.file,
+                                element,
+                                self.building,
+                                pset_topology["BackCellIndex"],
+                            )
 
     def build_trace(
         self,
@@ -603,6 +631,7 @@ class Molior:
                     "chain": chain,
                     "circulation": self.circulation,
                     "file": self.file,
+                    "building": self.building,
                     "name": name,
                     "elevation": elevation,
                     "height": height,
@@ -641,6 +670,7 @@ class Molior:
                     "cellcomplex": self.cellcomplex,
                     "elevations": self.elevations,
                     "file": self.file,
+                    "building": self.building,
                     "name": name,
                     "style": stylename,
                     "hull": hull,

molior/extrusion.py
@@ -145,7 +145,7 @@ class Extrusion(TraceClass):
             ),
         )
 
-        assign_storey_byindex(self.file, element, self.level)
+        assign_storey_byindex(self.file, element, self.building, self.level)
         directrix = self.path
         if self.closed:
             directrix.append(directrix[0])

molior/floor.py
@@ -66,7 +66,7 @@ class Floor(TraceClass):
             name=self.name + "/" + str(cell.Get("index")),
         )
         # Slab will be re-assigned to Space later
-        assign_storey_byindex(self.file, element, self.level)
+        assign_storey_byindex(self.file, element, self.building, self.level)
 
         if type(cell) == Cell:
             bottom_faces_ptr = []

molior/repeat.py
@@ -102,7 +102,7 @@ class Repeat(TraceClass):
                 predefined_type=self.predefined_type,
             )
             # assign the aggregate to a storey
-            assign_storey_byindex(self.file, aggregate, self.level)
+            assign_storey_byindex(self.file, aggregate, self.building, self.level)
 
             if self.parent_aggregate != None:
                 run(
@@ -148,6 +148,7 @@ class Repeat(TraceClass):
                                     "normal_set": self.normal_set,
                                     "parent_aggregate": aggregate,
                                     "style": self.style,
+                                    "building": self.building,
                                     "level": self.level,
                                     "predefined_type": self.predefined_type,
                                     "style_assets": self.style_assets,

molior/shell.py
@@ -221,4 +221,4 @@ class Shell(BaseClass):
         level = 0
         if elevation in self.elevations:
             level = self.elevations[elevation]
-        assign_storey_byindex(self.file, aggregate, level)
+        assign_storey_byindex(self.file, aggregate, self.building, level)

molior/space.py
@@ -67,7 +67,7 @@ class Space(TraceClass):
         # FIXME should create IfcSpaceType for this
         self.add_psets(element)
 
-        assign_storey_byindex(self.file, element, self.level)
+        assign_storey_byindex(self.file, element, self.building, self.level)
 
         if not self.do_representation:
             return

commit 74d128c283d34ebbbb751069eb25e92da0dceed3

fix bug introduced in 345af16 that didn't attach Elements to Spaces

molior/ifc/__init__.py
@@ -467,7 +467,7 @@ def get_building(entity):
         parents = entity.ContainedInStructure
         if not parents:
             return None
-        parent = parents[0]
+        parent = parents[0].RelatingStructure
     elif entity.is_a("IfcSpatialElement"):
         decomposes = entity.Decomposes
         if not decomposes:
@@ -477,7 +477,7 @@ def get_building(entity):
         return None
     if parent.is_a("IfcBuilding"):
         return parent
-    get_building(parent)
+    return get_building(parent)
 
 
 def get_material_by_name(self, subcontext, material_name, style_materials):