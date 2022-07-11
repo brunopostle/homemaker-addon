7a0e7c79e06c42d57f554f69deb0a1ee54ba1d53
Bruno Postle <bruno@postle.net>
2022-07-11 21:21:42 +0100

    configuration for buildthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,8 @@ release = '0.0'
 
 # -- General configuration ---------------------------------------------------
 
+autodoc_mock_imports = ["topologic", "ifcopenshell"]
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@ setup(
     packages=find_packages(
         include=["topologist", "topologist.*", "molior", "molior.*"]
     ),
-    install_requires=["topologicPy", "yaml", "ezdxf", "numpy", "ifcopenshell"],
+    install_requires=["topologic", "yaml", "ezdxf", "numpy", "ifcopenshell"],
 )
