433794fee468efab70d2b13a73b0ada3270b2ec3
Bruno Postle <bruno@postle.net>
2022-07-30 11:34:49 +0100

    test append_asset for materials

--- tests/test_ifcapi4.py
+++ tests/test_ifcapi4.py
@@ -4,12 +4,14 @@ import os
 import sys
 import unittest
 import ifcopenshell.api
+import ifcopenshell.util.element
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import molior.ifc
 from molior.ifc import (
     get_extruded_type_by_name,
     get_context_by_name,
+    add_pset,
 )
 
 run = ifcopenshell.api.run
@@ -65,6 +67,19 @@ class Tests(unittest.TestCase):
             relating_type=column_type,
         )
 
+        for material in self.file.by_type("IfcMaterial"):
+            add_pset(self.file, material, "some pset name", {"foo": "bar"})
+
+        # test copying materials
+        self.another_file = molior.ifc.init(name="My Other Project")
+        for entity in self.file.by_type("IfcMaterial"):
+            run(
+                "project.append_asset",
+                self.another_file,
+                library=self.file,
+                element=entity,
+            )
+
         # an extruded solid
         shape = self.file.createIfcShapeRepresentation(
             self.body_context,
@@ -95,6 +110,40 @@ class Tests(unittest.TestCase):
 
     def test_write(self):
         self.file.write("_test.ifc")
+
+    def test_copy_material(self):
+        self.assertEqual(len(self.file.by_type("IfcGeometricRepresentationContext")), 5)
+        self.assertEqual(
+            len(self.another_file.by_type("IfcGeometricRepresentationContext")), 5
+        )
+        for material in self.another_file.by_type("IfcMaterial"):
+            self.assertEqual(len(material.HasRepresentation), 1)
+            self.assertEqual(len(material.HasRepresentation[0].Representations), 1)
+            self.assertEqual(
+                len(material.HasRepresentation[0].Representations[0].Items), 1
+            )
+            self.assertEqual(
+                len(material.HasRepresentation[0].Representations[0].Items[0].Styles), 1
+            )
+            self.assertEqual(
+                len(
+                    material.HasRepresentation[0]
+                    .Representations[0]
+                    .Items[0]
+                    .Styles[0]
+                    .Styles
+                ),
+                1,
+            )
+            self.assertEqual(
+                material.HasRepresentation[0]
+                .Representations[0]
+                .Items[0]
+                .Styles[0]
+                .Styles[0]
+                .SurfaceColour.is_a(),
+                "IfcColourRgb",
+            )
 
 
 if __name__ == "__main__":
