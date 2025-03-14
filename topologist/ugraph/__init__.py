7109601c86a415876304059beb744b03ec5d77c4
Bruno Postle <bruno@postle.net>
2025-03-14 23:24:33 +0000

    ugraph: use sets where possible

--- topologist/ugraph/__init__.py
+++ topologist/ugraph/__init__.py
@@ -16,31 +16,36 @@ class graph:
 
     # FIXME doesn't appear to be in use
     def get_edge_data(self, edge):
-        if self.graph[edge[0]] and self.graph[edge[0]][0] == edge[1]:
+        if edge[0] in self.graph and self.graph[edge[0]][0] == edge[1]:
             return self.graph[edge[0]][1]
-        if self.graph[edge[1]] and self.graph[edge[1]][0] == edge[0]:
+        if edge[1] in self.graph and self.graph[edge[1]][0] == edge[0]:
             return self.graph[edge[1]][1]
         return None
 
     def nodes(self):
-        """Return node in use"""
-        result = {}
+        """Return nodes in use"""
+        result = set()
         for vertex in self.graph:
-            result[vertex] = True
-            result[self.graph[vertex][0]] = True
+            if self.graph[vertex]:
+                result.add(vertex)
+                result.add(self.graph[vertex][0])
         return result
 
     def edges(self):
         """Return edges as a list of node pairs"""
-        return [[vertex, self.graph[vertex][0]] for vertex in self.graph]
+        return [
+            [vertex, self.graph[vertex][0]]
+            for vertex in self.graph
+            if self.graph[vertex]
+        ]
 
     def starts(self):
         """Return a list of starting nodes"""
-        return [vertex_a for vertex_a in self.graph]
+        return [vertex for vertex in self.graph if self.graph[vertex]]
 
     def ends(self):
         """Return a list of ending nodes"""
-        return [self.graph[vertex_a][0] for vertex_a in self.graph]
+        return [self.graph[vertex][0] for vertex in self.graph if self.graph[vertex]]
 
     def is_simple_cycle(self):
         """Does the last node connect to the first node?"""
@@ -50,20 +55,15 @@ class graph:
 
     def source_vertices(self):
         """Return a list of starting nodes that are not ends"""
-        result = []
-        start_list = self.starts()
-        end_list = self.ends()
-        for start in start_list:
-            if start not in end_list:
-                result.append(start)
-        return result
+        start_set = set(self.starts())
+        end_set = set(self.ends())
+        return start_set - end_set
 
     def find_chains(self):
         """Return a list of open chains as new graph objects.
         Results are removed from this graph"""
         result = []
-        source_list = self.source_vertices()
-        for vertex in source_list:
+        for vertex in self.source_vertices():
             chain = graph()
             todo = True
             while todo is True:
@@ -83,21 +83,35 @@ class graph:
         Results are removed from this graph. This method assumes
         that find_chains() has already been run"""
         result = []
-        for vertex in self.graph:
-            if not self.graph[vertex]:
+        visited = set()
+
+        for vertex in list(self.graph.keys()):
+            if vertex in visited or not self.graph[vertex]:
                 continue
+
             cycle = graph()
-            todo = True
-            while todo is True:
-                if self.graph[vertex] and self.graph[vertex][0] in self.graph:
-                    cycle.add_edge({vertex: self.graph[vertex]})
-                    vertex_next = self.graph[vertex][0]
-                    self.graph[vertex] = False
-                    vertex = vertex_next
-                else:
-                    self.graph[vertex] = False
-                    todo = False
+            current = vertex
+            cycle_nodes = set()
+
+            while self.graph[current]:
+                cycle.add_edge({current: self.graph[current]})
+                visited.add(current)
+
+                next_vertex = self.graph[current][0]
+                self.graph[current] = False
+
+                if next_vertex in cycle_nodes:
+                    # We've found a cycle
+                    break
+
+                cycle_nodes.add(next_vertex)
+                current = next_vertex
+
+                if current not in self.graph or not self.graph[current]:
+                    break
+
             result.append(cycle)
+
         return result
 
     def find_paths(self):
