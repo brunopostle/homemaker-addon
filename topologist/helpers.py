62a0b51e125d97c4c9bd18925670d45049c8ac5f
Bruno Postle <bruno@postle.net>
2025-03-14 23:49:54 +0000

    use round() instead of home-rolled equivalent

--- topologist/helpers.py
+++ topologist/helpers.py
@@ -1,8 +1,32 @@
+import math
+
+
 def el(elevation):
-    if elevation >= 0.0:
-        return int((elevation * 1000) + 0.5) / 1000
-    return int((elevation * 1000) - 0.5) / 1000
+    """
+    Round an elevation value to 3 decimal places.
+
+    Args:
+        elevation (float): The elevation value to round
+
+    Returns:
+        float: The rounded elevation value
+    """
+    return round(elevation, 3)
 
 
 def string_to_coor(string):
-    return [float(num) for num in string.split("__")]
+    """
+    Convert a string representation of coordinates to a list of floats.
+
+    Args:
+        string (str): String in format "x__y__z"
+
+    Returns:
+        list: List of coordinates [x, y, z] as floats
+    """
+    try:
+        return [float(num) for num in string.split("__")]
+    except ValueError:
+        raise ValueError(
+            f"Invalid coordinate string format: {string}. Expected format: 'x__y__z'"
+        )
